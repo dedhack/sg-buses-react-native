814c380ea160639d1fd0b5c35c9eddb628549417
izharj <eezhar@gmail.com>
2023-03-21 23:16:03 +0800

    added react-navigation dependency

---
+++ sg-bus-arrivals/screens/NearbyScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const NearbyScreen = () => {
+  return (
+    <View>
+      <Text>NearbyScreen</Text>
+    </View>
+  );
+};
+
+export default NearbyScreen;
